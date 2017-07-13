6383c3f725a862cc3ae3d43a7050aab3e737c6ab
Joel Nilsson <jn4169@redbull.xdin.com>
2017-07-13 11:23:58 +0200

    troubleshooting loginmodule

--- src/main/java/com/kafka/PlainLoginModule.java
+++ src/main/java/com/kafka/PlainLoginModule.java
@@ -11,29 +11,42 @@ public class PlainLoginModule implements LoginModule {
     private static final String USERNAME_CONFIG = "username";
     private static final String PASSWORD_CONFIG = "password";
 
+    @Override
     public void initialize(Subject subject, CallbackHandler callbackHandler, Map<String, ?> sharedState, Map<String, ?> options) {
-        System.out.println("----------------------------------Working-----------------------------");
-
+        System.out.println("----------------------------------Working1-----------------------------");
         String username = (String) options.get(USERNAME_CONFIG);
+
+        for (Map.Entry<String, ?> value : options.entrySet()) {
+            System.out.println(value.getKey());
+            System.out.println(value.getValue());
+        }
+
         if (username != null)
-            subject.getPublicCredentials().add(username);
+            System.out.println("----------------------------------Working2-----------------------------");
+        subject.getPublicCredentials().add(username);
         String password = (String) options.get(PASSWORD_CONFIG);
         if (password != null)
-            subject.getPrivateCredentials().add(password);
+            System.out.println("----------------------------------Working3-----------------------------");
+        subject.getPrivateCredentials().add(password);
+        System.out.println("----------------------------------Working4-----------------------------");
     }
 
+    @Override
     public boolean login() throws LoginException {
         return true;
     }
 
+    @Override
     public boolean logout() throws LoginException {
         return true;
     }
 
+    @Override
     public boolean commit() throws LoginException {
         return true;
     }
 
+    @Override
     public boolean abort() throws LoginException {
         return false;
     }
